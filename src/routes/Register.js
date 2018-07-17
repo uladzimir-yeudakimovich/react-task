de2db3e2ddb2d2d8849cd0c63582acd4a8319a21
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-17 04:11:17 +0300

    added Register

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FormControl}  from "react-bootstrap";
+import { FormControl, Checkbox, Button }  from "react-bootstrap";
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 export default class Register extends React.Component {
   constructor() {
@@ -9,15 +11,25 @@ export default class Register extends React.Component {
       email: '',
       password: '',
       isManager: false,
-      isDeveloper: false,
     };
   }
 
   onChange = (e) => {
-    const value = e.target.checked === undefined ? e.target.value: e.target.checked;
-    this.setState({
-      [e.target.name]: value,
-    })
+    if (e.target.name === 'isManager') {
+      this.setState({
+        [e.target.name]: e.target.checked,
+      })
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      })
+    }
+  }
+
+  onSubmit = async (e) => {
+    const response = await this.props.mutate({
+      variables: this.state,
+    });
   }
 
   render() {
@@ -39,8 +51,31 @@ export default class Register extends React.Component {
           type="password"
           onChange={e => this.onChange(e)}
           value={this.state.password} />
+        <Checkbox
+          name="isManager"
+          checked={this.state.isManager}
+          onChange={e => this.onChange(e)}
+        >
+          Manager?
+        </Checkbox>
+        <Button
+          bsStyle="primary"
+          onClick={() => this.onSubmit()}
+        >
+          Submit
+        </Button>
       </div>
     );
   }
 }
+
+const mutation = gql`
+  mutation($username: String, $email: String!, password: String!, $isManager: Boolean) {
+    register(username: $username, email: $email, password: $password, isManager: $isManager) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(Register);
 
