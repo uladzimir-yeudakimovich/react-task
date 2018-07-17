00ca30a56007b154c75a334a7441b831906bea2c
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-18 00:39:06 +0300

    add button add

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import TodoList from './TodoList';
 
 export default class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+
   render() {
     return (
       <div className="container">
@@ -12,7 +39,18 @@ export default class Projects extends React.Component {
           <ListGroupItem>Project 3</ListGroupItem>
           <ListGroupItem>Project 4</ListGroupItem>
           <ListGroupItem>Project 5</ListGroupItem>
+          <ListGroupItem>Project 5</ListGroupItem>
+          <TodoList items={this.state.items} />
         </ListGroup>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button className="btn-primary">Add</button>
+        </form>
       </div>
     );
   }
--- src/routes/Tasks.js
+++ src/routes/Tasks.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import TodoList from './TodoList';
 
 export default class Tasks extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            id: '',
-            task: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    onChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
     }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+
+  handleRemove() {
+    console.log('delete');
+  }
 
 	render() {
 		return (
 			<div className="container">
 				<h1>Tasks</h1>
 				<ListGroup>
-					<ListGroupItem>Task 1</ListGroupItem>
+					<ListGroupItem>Task 1<button onClick={this.handleRemove}>delete</button></ListGroupItem>
 					<ListGroupItem>Task 2</ListGroupItem>
 					<ListGroupItem>Task 3</ListGroupItem>
 					<ListGroupItem>Task 4</ListGroupItem>
@@ -30,8 +47,18 @@ export default class Tasks extends React.Component {
 					<ListGroupItem>Task 7</ListGroupItem>
 					<ListGroupItem>Task 8</ListGroupItem>
 					<ListGroupItem>Task 9</ListGroupItem>
-					<ListGroupItem>Task 10</ListGroupItem>
-				</ListGroup>
+          <ListGroupItem>Task 10</ListGroupItem>
+          <TodoList items={this.state.items} />
+        </ListGroup>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button className="btn-primary">Add</button>
+        </form>
 			</div>
 		);
 }
