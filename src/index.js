54c32355c0a1c7a129b5d596bd8cb6b65f335b45
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-07-17 21:44:39 +0300

    add photo

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { Navbar }  from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Routes from './routes';
+import './main.css';
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: 'http://localhost:3000/graphql' }),
@@ -18,14 +19,14 @@ const client = new ApolloClient({
 
 const App = () => (
 	<ApolloProvider client={client}>
-		<Navbar>
+		<Navbar className="nav">
 			<a href="/">Home</a>
 			<a href="registration">Registration</a>
 			<a href="login">Login</a>
 			<a href="projects">Projects</a>
 			<a href="tasks">Tasks</a>
-		</Navbar>
-		<Routes />
+    </Navbar>
+    <Routes />
 	</ApolloProvider>
 );
 
