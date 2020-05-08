512ef0878664bbea6553be613ee409db84a59421
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-05-09 02:27:12 +0300

    added error-handling

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ const { showRequestLogger, saveRequestLogger } = require('./middleware/logger');
 const infoRouter = require('./routers/info/info.router');
 const personsRouter = require('./routers/persons/persons.router');
 const unknownEndpoint = require('./middleware/unknown-endpoint');
+const errorHandler = require('./middleware/error-handler');
 
 const app = express();
 
-app.use(cors());
+app.use(express.static('build'));
 app.use(express.json());
 app.use(showRequestLogger);
 app.use(saveRequestLogger);
+app.use(cors());
 
 app.use('/', (req, res, next) => {
   if (req.originalUrl === '/') return res.send('Service is running!');
@@ -20,6 +22,8 @@ app.use('/', (req, res, next) => {
 
 app.use('/info', infoRouter);
 app.use('/api/persons', personsRouter);
+
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
--- src/routers/persons/persons.router.js
+++ src/routers/persons/persons.router.js
@@ -3,60 +3,80 @@ const router = require('express').Router();
 const personsService = require('./persons.service');
 const User = require('../../models/user.model');
 
-router.route('/').get(async (req, res) => {
-  const persons = await personsService.getAll();
-  res.json(persons.map(User.toResponse));
+router.route('/').get(async (req, res, next) => {
+  await personsService
+    .getAll()
+    .then(result => {
+      res.json(result.map(User.toResponse));
+    })
+    .catch(error => next(error));
 });
 
-router.route('/:id').get(async (req, res) => {
+router.route('/:id').get(async (req, res, next) => {
   const id = Number(req.params.id);
-  const person = await personsService.getUser(id);
-  if (person) {
-    res.json(User.toResponse(person));
-  } else {
-    res.status(404).end();
-  }
+  await personsService
+    .getUser(id)
+    .then(result => {
+      if (result) {
+        res.json(User.toResponse(result));
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-router.route('/').post(async (req, res) => {
+router.route('/').post(async (req, res, next) => {
   const { body } = req;
   const { name, number } = body;
   if (!name || !number) {
     return res.status(400).send({ error: 'content missing' });
   }
-  const person = await personsService.addUser(body);
-  if (person) {
-    res.json(User.toResponse(person));
-  } else {
-    res
-      .status(409)
-      .send({ error: 'name must be unique' })
-      .end();
-  }
+  await personsService
+    .addUser(body)
+    .then(result => {
+      if (result) {
+        res.json(User.toResponse(result));
+      } else {
+        res
+          .status(409)
+          .send({ error: 'name must be unique' })
+          .end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-router.route('/:id').put(async (req, res) => {
+router.route('/:id').put(async (req, res, next) => {
   const id = Number(req.params.id);
   const { body } = req;
   const { name, number } = body;
   if (!name || !number) {
     return res.status(400).send({ error: 'content missing' });
   }
-  const person = await personsService.editUser(id, body);
-  if (person) {
-    res.json(User.toResponse(person));
-  } else {
-    res
-      .status(409)
-      .send({ error: 'name must be unique' })
-      .end();
-  }
+  await personsService
+    .editUser(id, body)
+    .then(result => {
+      if (result) {
+        res.json(User.toResponse(result));
+      } else {
+        res
+          .status(409)
+          .send({ error: 'name must be unique' })
+          .end();
+      }
+    })
+    .catch(error => next(error));
 });
 
-router.route('/:id').delete(async (req, res) => {
+router.route('/:id').delete(async (req, res, next) => {
   const id = Number(req.params.id);
-  await personsService.deleteUser(id);
-  res.status(204).end();
+  await personsService
+    .deleteUser(id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 module.exports = router;
