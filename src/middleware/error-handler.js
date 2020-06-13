a5c4227dc83400556224911795a845945b27a406
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-06-13 21:56:26 +0300

    improved validation res

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -5,11 +5,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (err.name === 'ValidationError') {
-    return res.status(400).send({
-      name: 'Validation failed',
-      status: res.statusCode,
-      message: err.message
-    });
+    return res.status(400).json({ error: { message: err.message } });
   } else if (err.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'invalid token' });
   }
