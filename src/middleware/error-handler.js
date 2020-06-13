78eeaab9e6fa81e841fbfc13a3743b01d4353ae7
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-06-13 22:18:30 +0300

    updated valed res

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (err.name === 'ValidationError') {
-    return res.json({
+    return res.status(400).json({
       name: 'Validation failed',
       status: res.statusCode,
       message: err.message
